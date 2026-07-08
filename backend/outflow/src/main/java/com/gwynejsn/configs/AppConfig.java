1b00d98d86872aec939af4811a97cda2ea812f5d
Gwyne Justin Deterala <gwynejustin.deterala.p@gmail.com>
2026-07-08 13:45:30 +0800

    add: initialized maven project

---
+++ backend/outflow/src/main/java/com/gwynejsn/configs/AppConfig.java
@@ -0,0 +1,9 @@
+package com.gwynejsn.configs;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "com.gwynejsn")
+public class AppConfig {
+}
