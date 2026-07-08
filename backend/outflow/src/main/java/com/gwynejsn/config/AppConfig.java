5bf8a9c9cec87dbc1de4e3163b00030b0284c89c
Gwyne Justin Deterala <gwynejustin.deterala.p@gmail.com>
2026-07-08 17:11:01 +0800

    feat: added spring data jpa with hikariCP as datasource

---
+++ backend/outflow/src/main/java/com/gwynejsn/config/AppConfig.java
@@ -0,0 +1,52 @@
+package com.gwynejsn.config;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import jakarta.persistence.EntityManagerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import javax.sql.DataSource;
+
+@Configuration
+@ComponentScan(basePackages = "com.gwynejsn")
+@EnableTransactionManagement
+@EnableJpaRepositories(basePackages = "com.gwynejsn.dao")
+public class AppConfig {
+
+    @Bean
+    public DataSource dataSource() {
+        HikariConfig config = new HikariConfig();
+        config.setDriverClassName("org.h2.Driver");
+        config.setJdbcUrl("jdbc:h2:mem:testdb");
+        config.setUsername("sa");
+        config.setPassword("");
+        return new HikariDataSource(config);
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+        HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+        vendorAdapter.setGenerateDdl(true);
+
+        LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
+        em.setDataSource(dataSource());
+        em.setJpaVendorAdapter(vendorAdapter);
+        em.setPackagesToScan("com.gwynejsn.model");
+        return em;
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager(EntityManagerFactory emf) {
+        JpaTransactionManager transactionManager = new JpaTransactionManager();
+        transactionManager.setEntityManagerFactory(emf);
+        return transactionManager;
+    }
+}
---
+++ backend/outflow/src/main/java/com/gwynejsn/model/Subscription.java
@@ -0,0 +1,100 @@
+package com.gwynejsn.model;
+
+import com.gwynejsn.enums.Category;
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+public class Subscription {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    private String title;
+    private String description;
+    private Float price;
+    @Column(name = "image_url")
+    private String imageUrl;
+    private Category category;
+    private LocalDateTime createdAt;
+    private LocalDateTime renewedAt;
+    private LocalDateTime expiresAt;
+
+    public Subscription() {}
+
+    public Subscription(String title, String description, Float price, String imageUrl, Category category, LocalDateTime createdAt, LocalDateTime renewedAt, LocalDateTime expiresAt) {
+        this.title = title;
+        this.description = description;
+        this.price = price;
+        this.imageUrl = imageUrl;
+        this.category = category;
+        this.createdAt = createdAt;
+        this.renewedAt = renewedAt;
+        this.expiresAt = expiresAt;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Float getPrice() {
+        return price;
+    }
+
+    public void setPrice(Float price) {
+        this.price = price;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getRenewedAt() {
+        return renewedAt;
+    }
+
+    public void setRenewedAt(LocalDateTime renewedAt) {
+        this.renewedAt = renewedAt;
+    }
+
+    public LocalDateTime getExpiresAt() {
+        return expiresAt;
+    }
+
+    public void setExpiresAt(LocalDateTime expiresAt) {
+        this.expiresAt = expiresAt;
+    }
+}
